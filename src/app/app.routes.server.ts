55e66b02b3f393fb6e8686d111cc126c5be6ea46
Kiril Burlaka <kiril302005@gmaoil.com>
2026-06-20 19:18:52 +0200

    cleanup + tailwindcss setup

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    // We'll have a single page and since it starts with empty input, we can prerender it.
     path: '**',
     renderMode: RenderMode.Prerender,
   },
