86bbd3491e9bb1143e5fc0a9efa49136739cd591
Kiril Burlaka <kiril302005@gmaoil.com>
2026-06-20 18:24:08 +0200

    fix: allowedHosts: [localhost]

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,9 @@ import { join } from 'node:path';
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+const angularApp = new AngularNodeAppEngine({
+  allowedHosts: ['localhost:4000', 'localhost']
+});
 
 /**
  * Example Express Rest API endpoints can be defined here.
